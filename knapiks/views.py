daa56ff2305a7629f9dab2334b9b099c5f394547
efstone <evan@aquapony.com>
2019-08-09 11:30:29 -0700

    cross-site request forgery protection got in the way

--- knapiks/views.py
+++ knapiks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 
+@csrf_exempt
 def receive_message(request):
     with open('/home/opendata/message_request.txt', 'wb') as f:
         f.write(request.body)
