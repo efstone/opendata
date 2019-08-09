cb51bd07286425429e71cd608debbb6b3034e314
efstone <evan@aquapony.com>
2019-08-09 09:52:18 -0700

    added twilio receive test

--- docketdata/urls.py
+++ docketdata/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from knapiks.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mc_chat/', receive_message)
 ]
---
+++ knapiks/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def receive_message(request):
+    with open('/home/opendata/message_request.txt', 'w') as f:
+        f.write(request)
