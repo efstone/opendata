80afd417dac10de29230c6da057b04eec085e428
efstone <evan@aquapony.com>
2019-08-09 09:59:33 -0700

    tweaking twilio receive test

--- knapiks/views.py
+++ knapiks/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def receive_message(request):
     with open('/home/opendata/message_request.txt', 'w') as f:
-        f.write(request)
+        body = request.values.get('Body', None)
+        f.write(body)
