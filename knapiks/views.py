0db39dea1cc7e7f587f12b58398830cea49015e9
efstone <evan@aquapony.com>
2019-08-09 11:48:58 -0700

    message transference test

--- knapiks/views.py
+++ knapiks/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from knapiks.mcchat import *
+from twilio.twiml.messaging_response import MessagingResponse
+
 
 # Create your views here.
 
 
 @csrf_exempt
 def receive_message(request):
-    message = {}
-    message['From'] = request.POST['From']
-    message['Body'] = request.POST['Body']
-    with open('/home/opendata/message_request.txt', 'w') as f:
-        f.write(f"{message}")
-    return render(request, "base.html", {'request': request.body})
+    if request.POST['From'] == '+19405947406':
+        login_and_send(request.POST['Body'])
+    else:
+        resp = MessagingResponse()
+        resp.message("This number cannot receive messages at this time.")
+    return render(request, "base.html")
