0d6053616e3b284c9e5e80fc8ec11e95c385e828
efstone <evan@aquapony.com>
2019-08-09 12:46:58 -0700

    some error checking

--- knapiks/views.py
+++ knapiks/views.py
@@ -9,10 +9,13 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 @csrf_exempt
 def receive_message(request):
-    if request.POST['From'] == '+19405947406':
-        login_and_send(request.POST['Body'])
-        return
-    else:
-        resp = MessagingResponse()
-        resp.message("This number cannot receive messages at this time.")
-        return f"{resp}"
+    try:
+        if request.POST['From'] == '+19405947406':
+            login_and_send(request.POST['Body'])
+            return
+        else:
+            resp = MessagingResponse()
+            resp.message("This number cannot receive messages at this time.")
+            return f"{resp}"
+    except Exception as e:
+        return render(request, "base.html", {'error': e})
