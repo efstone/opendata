f2714cb48480d5ecc697e7504ca1b1a0e2e72fe0
efstone <evan@aquapony.com>
2019-11-21 12:08:21 -0600

    updates to mcchat for botania server (forge 1.12)

--- knapiks/mcchat.py
+++ knapiks/mcchat.py
@@ -54,7 +54,7 @@ def get_latest_log():
     mc_ftp.login(ftp_login, mc_decrypt(ftp_pw_crypt, mc_key))
     mc_ftp.prot_p()
     mc_log = []
-    mc_ftp.retrlines('RETR /minecraft/logs/latest.log', mc_log.append)
+    mc_ftp.retrlines('RETR /custom-minecraft/logs/latest.log', mc_log.append)
     return mc_log
 
 
@@ -68,7 +68,10 @@ def login_and_send(command):
         cmd = mcrcon.command(mc, command)
         return cmd
     except Exception as e:
-        mc.close()
+        try:
+            mc.close()
+        except:
+            pass
         return e
 
 
